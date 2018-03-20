84a4077550e292a1cb5523664eecb300c4365b62
Federico <federico.rizzo93@gmail.com>
2018-03-20 17:24:22 +0100

    Format code

--- app/src/main/java/com/example/android/lab1/EditProfileActivity.java
+++ app/src/main/java/com/example/android/lab1/EditProfileActivity.java
@@ -175,16 +175,15 @@ public class EditProfileActivity extends AppCompatActivity {
 
     public File saveThumbnail() throws IOException{
 
-        // Create an image file name
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            String imageFileName = "JPEG_" + timeStamp + "_";
-            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            File image = File.createTempFile(
-                    imageFileName,  /* prefix */
-                    ".jpg",         /* suffix */
-                    storageDir      /* directory */
-            );
-            mCurrentPhotoPath = image.getAbsolutePath();
-            return image;
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
