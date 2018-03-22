ffaace89df6e97ff67855327e199cc1fb0ac8fd3
Federico <federico.rizzo93@gmail.com>
2018-03-22 12:54:46 +0100

    Add max length to phone form

--- app/src/main/java/com/example/android/lab1/MainActivity.java
+++ app/src/main/java/com/example/android/lab1/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ImageView;
 
