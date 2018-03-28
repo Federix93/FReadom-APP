cddf125b5229d85ba7bd031b0156e1cdc32670f6
Federico <federico.rizzo93@gmail.com>
2018-03-28 12:52:56 +0200

    Prevent activities open twice

--- app/src/main/java/com/example/android/lab1/EditProfileActivity.java
+++ app/src/main/java/com/example/android/lab1/EditProfileActivity.java
@@ -4,13 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -40,6 +38,7 @@ public class EditProfileActivity extends AppCompatActivity {
     private final int RESULT_LOAD_IMAGE = 1;
     private final int CAPTURE_IMAGE = 0;
     String mCurrentPhotoPath;
+    private AlertDialog.Builder mAlertDialogBuilder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -118,7 +117,7 @@ public class EditProfileActivity extends AppCompatActivity {
                 user.setImage(mCurrentPhotoPath);
                 sharedPreferencesManager.putUser(user);
                 Intent intent = new Intent(view.getContext(), MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
             }
         });
@@ -126,16 +125,15 @@ public class EditProfileActivity extends AppCompatActivity {
         mCameraImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 final CharSequence[] items = { getString(R.string.camera_option_dialog) ,getString(R.string.gallery_option_dialog)};
-                final AlertDialog.Builder dialog = new AlertDialog.Builder(view.getContext());
-                dialog.setNegativeButton(R.string.negative_button_dialog, new DialogInterface.OnClickListener() {
+                mAlertDialogBuilder = new AlertDialog.Builder(view.getContext());
+                mAlertDialogBuilder.setNegativeButton(R.string.negative_button_dialog, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         dialogInterface.cancel();
                     }
                 });
-                dialog.setTitle(R.string.title_dialog).setItems(items, new DialogInterface.OnClickListener() {
+                mAlertDialogBuilder.setTitle(R.string.title_dialog).setItems(items, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         switch(i) {
@@ -174,6 +172,7 @@ public class EditProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), PositionActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
             }
         });
@@ -182,6 +181,7 @@ public class EditProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), PositionActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
             }
         });
