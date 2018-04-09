ec4bab42e7c80ead186f665291a39bbcdcf66d90
Federico <federico.rizzo93@gmail.com>
2018-04-09 22:53:31 +0200

    Add login with google and email/password

--- app/src/main/java/com/example/android/lab1/MainActivity.java
+++ app/src/main/java/com/example/android/lab1/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.android.lab1;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
 
@@ -25,12 +32,18 @@ public class MainActivity extends AppCompatActivity {
     ImageView mCircleImageView;
     private Toolbar mToolbar;
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
     public static final String ADDRESS_KEY = "ADDRESS";
+    private static final int RC_SIGN_IN = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         mUsernameTextInputLayout = findViewById(R.id.username_text);
 
@@ -55,43 +68,43 @@ public class MainActivity extends AppCompatActivity {
         User user = sharedPreferencesManager.getUser();
 
         if (mUsernameTextInputLayout.getEditText() != null) {
-            if(user != null)
+            if (user != null)
                 mUsernameTextInputLayout.getEditText().setText(user.getUsername());
             mUsernameTextInputLayout.getEditText().setKeyListener(null);
             mUsernameTextInputLayout.getEditText().setEnabled(false);
         }
         if (mEmailTextInputLayout.getEditText() != null) {
-            if(user != null)
+            if (user != null)
                 mEmailTextInputLayout.getEditText().setText(user.getEmail());
             mEmailTextInputLayout.getEditText().setKeyListener(null);
             mEmailTextInputLayout.getEditText().setEnabled(false);
         }
         if (mPhoneTextInputLayout.getEditText() != null) {
-            if(user != null)
+            if (user != null)
                 mPhoneTextInputLayout.getEditText().setText(user.getPhone());
             mPhoneTextInputLayout.getEditText().setKeyListener(null);
             mPhoneTextInputLayout.getEditText().setEnabled(false);
         }
 
-        if(mShortBioTextInputLayout.getEditText() != null){
-            if(user != null)
+        if (mShortBioTextInputLayout.getEditText() != null) {
+            if (user != null)
                 mShortBioTextInputLayout.getEditText().setText(user.getShortBio());
             mShortBioTextInputLayout.getEditText().setKeyListener(null);
             mShortBioTextInputLayout.getEditText().setEnabled(false);
         }
 
-        if(user != null && user.getImage() != null) {
+        if (user != null && user.getImage() != null) {
             Glide.with(this).load(user.getImage())
                     .apply(bitmapTransform(new CircleCrop()))
                     .into(mCircleImageView);
-        }else{
+        } else {
             Glide.with(this).load(getResources().getDrawable(R.drawable.ic_account_circle_black_24dp))
                     .apply(bitmapTransform(new CircleCrop()))
                     .into(mCircleImageView);
         }
 
-        if(mAddressTextInputLayout.getEditText() != null){
-            if(user != null)
+        if (mAddressTextInputLayout.getEditText() != null) {
+            if (user != null)
                 mAddressTextInputLayout.getEditText().setText(user.getAddress());
             mAddressTextInputLayout.getEditText().setKeyListener(null);
             mAddressTextInputLayout.getEditText().setEnabled(false);
@@ -109,5 +122,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(MainActivity.this,"VINCENZO SEI UNA CHECCA", Toast.LENGTH_LONG).show();
+                } else {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
