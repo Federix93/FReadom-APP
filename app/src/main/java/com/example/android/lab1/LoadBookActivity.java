17acd5729a93c65c8ca05a808208b02efdd50a97
Franc <francescino93@gmail.com>
2018-04-10 01:59:31 +0200

    - activity load book remade with constraint layout

--- app/src/main/java/com/example/android/lab1/LoadBookActivity.java
+++ app/src/main/java/com/example/android/lab1/LoadBookActivity.java
@@ -1,8 +1,25 @@
 package com.example.android.lab1;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageSwitcher;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class LoadBookActivity
+public class LoadBookActivity extends Activity
 {
-    ImageSwitcher mGallery;
+    private ImageSwitcher mGallery;
+    private EditText      mIsbnEditText;
+    private EditText      mTitleEditText;
+    private EditText      mAuthorEditText;
+    private EditText      mPublisherEditText;
+    private TextView      mPublishYearTextView;
+    private Spinner       mPublishYearSpinner;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
 }
