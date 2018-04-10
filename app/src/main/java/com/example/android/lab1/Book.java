514aaa426e462afd4eaa022800d584e0aad0563b
Franc <francescino93@gmail.com>
2018-04-10 15:03:30 +0200

    - model classes for book and book condition - loadBookActivity know validate isbn input and if it's valid it can also download information from google api

---
+++ app/src/main/java/com/example/android/lab1/Book.java
@@ -0,0 +1,140 @@
+package com.example.android.lab1;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public class Book implements Parcelable
+{
+    private String     isbn;
+    private String     title;
+    private String     author;
+    private String     publisher;
+    private int        publishYear;
+    private Condition  conditions;
+    private LatLng     position;
+
+    private String thumbnail;
+    private List<String> userPhotosPath;
+
+    public Book(String isbn, String title, String author, String publisher, int publishYear, Condition conditions, LatLng position) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.publishYear = publishYear;
+        this.conditions = conditions;
+        this.position = position;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public List<String> getUserPhotosPath() {
+        return userPhotosPath;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public Condition getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(Condition conditions) {
+        this.conditions = conditions;
+    }
+
+    public LatLng getPosition() {
+        return position;
+    }
+
+    public void setPosition(LatLng position) {
+        this.position = position;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[] {
+                isbn,
+                title,
+                author,
+                publisher,
+                Integer.toString(publishYear),
+                conditions.getStatus()
+        });
+        dest.writeParcelable(position, flags);
+    }
+
+    public static final Parcelable.Creator<Book> CREATOR = new Parcelable.Creator(){
+
+        @Override
+        public Book createFromParcel(Parcel source) {
+            String[] stringFields = new String[6];
+            source.readStringArray(stringFields);
+            LatLng latLng = source.readParcelable(LatLng.class.getClassLoader());
+            return new Book(stringFields[0],
+                    stringFields[1],
+                    stringFields[2],
+                    stringFields[3],
+                    Integer.parseInt(stringFields[4]),
+                    new Condition(stringFields[5]),
+                    latLng);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+}
--- app/src/main/java/com/example/android/lab1/LoadBookActivity.java
+++ app/src/main/java/com/example/android/lab1/LoadBookActivity.java
@@ -1,25 +1,237 @@
 package com.example.android.lab1;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoadBookActivity extends Activity
-{
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static android.provider.CalendarContract.CalendarCache.URI;
+
+public class LoadBookActivity extends Activity {
+    private static final String ISBN_REQUEST_TAG = "ISBN_REQUEST";
     private ImageSwitcher mGallery;
-    private EditText      mIsbnEditText;
-    private EditText      mTitleEditText;
-    private EditText      mAuthorEditText;
-    private EditText      mPublisherEditText;
-    private TextView      mPublishYearTextView;
-    private Spinner       mPublishYearSpinner;
+    private ImageView mAddImageIcon;
+    private EditText mIsbnEditText;
+    private ImageView mIsbnImageView;
+    private EditText mTitleEditText;
+    private EditText mAuthorEditText;
+    private EditText mPublisherEditText;
+    private TextView mPublishYearTextView;
+    private Spinner mPublishYearSpinner;
+    private TextView mConditionsTextView;
+    private Spinner mConditionsSpinner;
+    private EditText mPositionEditText;
+    private ImageView mPositionIcon;
+    private String mWebThumbnail;
+    private RequestQueue mRequestQueue;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_book);
+
+        mGallery = findViewById(R.id.load_book_gallery);
+        mAddImageIcon = findViewById(R.id.add_image_icon);
+        mIsbnEditText = findViewById(R.id.load_book_isbn);
+        mIsbnImageView = findViewById(R.id.load_book_auto_fill);
+        mTitleEditText = findViewById(R.id.load_book_title);
+        mAuthorEditText = findViewById(R.id.load_book_author);
+        mPublisherEditText = findViewById(R.id.load_book_publisher);
+        mPublishYearTextView = findViewById(R.id.load_book_publish_year_text_view);
+        mPublishYearSpinner = findViewById(R.id.load_book_publish_year_spinner);
+        mConditionsTextView = findViewById(R.id.load_book_conditions);
+        mConditionsSpinner = findViewById(R.id.load_book_conditions_spinner);
+        mPositionEditText = findViewById(R.id.load_book_position);
+        mPositionIcon = findViewById(R.id.position_image_view);
+
+        // spinners
+
+        // year spinner
+        String years[] = new String[200];
+
+        for (int i = Calendar.getInstance().get(Calendar.YEAR), n = 0; n < 200; i--, n++)
+            years[n] = Integer.toString(i);
+
+        mPublishYearSpinner.setAdapter(makeDropDownAdapter(years));
+
+        Condition.Status[] status = Condition.Status.values();
+        String conditions[] = new String[status.length];
+        int i = 0;
+        for (Condition.Status s : status) {
+            conditions[i++] = getString(Condition.getResource(s));
+        }
+
+        mConditionsSpinner.setAdapter(makeDropDownAdapter(conditions));
+
+        // TODO load gallery
+
+        // ISBN auto fill listener
+        mIsbnImageView.setVisibility(View.INVISIBLE);
+        mIsbnEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus && v instanceof EditText) {
+                    EditText isbnView = (EditText) v;
+                    validateIsbn(isbnView);
+                }
+            }
+        });
+
+        mIsbnEditText.setFilters(new InputFilter[]{
+                new InputFilter() {
+                    Pattern mPattern = Pattern.compile("^[-0123456789]");
+
+                    @Override
+                    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                        Matcher m = mPattern.matcher(source);
+                        return m.matches() ? null : "";
+                    }
+                }
+        });
+
+
+        mIsbnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validateIsbn(mIsbnEditText)) {
+                    Log.d("AUTOFILL", "onClick: autofill " + mIsbnEditText.getText());
+                    String isbnApiUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + mIsbnEditText.getText()
+                            .toString()
+                            .replace("-",
+                                    "");
+                    JsonObjectRequest jsonRequest = new JsonObjectRequest(
+                            Request.Method.GET, isbnApiUrl, null, new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Log.d("AUTOFILL", "onResponse: recieved response");
+                                if (response.getInt("totalItems") > 0) {
+                                    // take item 0
+                                    Log.d("AUTOFILL", "onResponse: has items");
+                                    JSONObject book = response.getJSONArray("items")
+                                            .getJSONObject(0).getJSONObject("volumeInfo");
+
+                                        mTitleEditText.setText(book.getString("title"));
+                                    if (book.has("authors") &&
+                                            book.getJSONArray("authors").length() > 0)
+                                        mAuthorEditText.setText(book.getJSONArray("authors")
+                                                .getString(0));
+                                    if (book.has("publisher"))
+                                        mPublisherEditText.setText(book.getString("publisher"));
+
+                                    if (book.has("publishedDate")) {
+                                        int currentYear, publishYear;
+                                        currentYear = Calendar.getInstance().get(Calendar.YEAR);
+                                        publishYear = book.getInt("publishedDate");
+                                        mPublishYearSpinner.setSelection(currentYear - publishYear);
+                                    }
+
+                                    if (book.has("imageLinks")) {
+                                        JSONObject imageLinks = book.getJSONObject("imageLinks");
+                                        if (imageLinks.length() > 0 &&
+                                                imageLinks.has("thumbnail")) {
+                                            mWebThumbnail = imageLinks.getString("thumbnail");
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Book not found",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), "ERROR IN REQUEST", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    jsonRequest.setTag(ISBN_REQUEST_TAG);
+                    if (mRequestQueue == null)
+                        mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+                    mRequestQueue.add(jsonRequest);
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        if (mRequestQueue != null)
+            mRequestQueue.cancelAll(ISBN_REQUEST_TAG);
+    }
+
+    private ArrayAdapter<String> makeDropDownAdapter(String[] items) {
+        List<String> temp;
+        temp = new ArrayList<>(Arrays.asList(items));
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item,
+                temp);
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        return arrayAdapter;
+    }
+
+    private boolean validateIsbn(EditText isbnView) {
+        if (isbnView.getText().length() > 0) {
+            Pattern isbn = Pattern.compile("(?:\\d-?){13}");
+            Matcher matcher = isbn.matcher(isbnView.getText().toString());
+            if (!matcher.matches()) {
+                String errorMessage = getString(R.string.invalid_isbn);
+                isbnView.setError(errorMessage);
+                mIsbnImageView.setVisibility(View.INVISIBLE);
+                return false;
+            } else {
+                isbnView.setError(null);
+                mIsbnImageView.setVisibility(View.VISIBLE);
+                return true;
+            }
+        } else {
+            isbnView.setError(null);
+            mIsbnImageView.setVisibility(View.INVISIBLE);
+        }
+        return false;
     }
 }
