f889cbb8feac4640cd303f8585ac9370d5feb963
mdp11 <mattia.dipi@gmail.com>
2018-04-16 12:53:22 +0200

    New layout for recycler view: each item is a card view and has a 3 dot menu

--- app/src/main/java/com/example/android/lab1/HomeFragment.java
+++ app/src/main/java/com/example/android/lab1/HomeFragment.java
@@ -1,16 +1,24 @@
 package com.example.android.lab1;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,9 +29,10 @@ import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
 
-    private static final int numColumns = 3;
+    private int mNumColumns;
 
-    private int numberOfItems;
+    private int mNumberOfItems;
+    private View mRootView;
     private RecyclerView mRecyclerView;
     private RecyclerBookAdapter mAdapter;
     private FragmentActivity mParent;
@@ -39,6 +48,11 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        if (container.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            mNumColumns = 2;
+        } else {
+            mNumColumns = 3;
+        }
 
         mToolbar = getActivity().findViewById(R.id.toolbar_main_activity);
         mToolbar.setTitle("Lab 2");
@@ -67,19 +81,64 @@ public class HomeFragment extends Fragment {
         mFAB.setVisibility(View.VISIBLE);
 
         mParent = getActivity();
-        numberOfItems = 200;
+        mNumberOfItems = 200;
 
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        mRootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        mRecyclerView = rootView.findViewById(R.id.recycler_books);
+        mRecyclerView = mRootView.findViewById(R.id.recycler_books);
 
-        GridLayoutManager layoutManager = new GridLayoutManager(mParent, numColumns);
+        GridLayoutManager layoutManager = new GridLayoutManager(mParent, mNumColumns);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setHasFixedSize(true);
-        mAdapter = new RecyclerBookAdapter(numberOfItems);
+        mRecyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mAdapter = new RecyclerBookAdapter(mRootView.getContext(), mNumberOfItems);
         mRecyclerView.setAdapter(mAdapter);
 
-        return rootView;
+        return mRootView;
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 
 
--- app/src/main/java/com/example/android/lab1/RecyclerBookAdapter.java
+++ app/src/main/java/com/example/android/lab1/RecyclerBookAdapter.java
@@ -2,67 +2,117 @@ package com.example.android.lab1;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
 public class RecyclerBookAdapter extends RecyclerView.Adapter<RecyclerBookAdapter.BookViewHolder> {
 
+    private Context mContext;
     private int mNumberItems;
 
-    public RecyclerBookAdapter(int nItems)
+    public RecyclerBookAdapter(Context context, int nItems)
     {
+        mContext = context;
         mNumberItems = nItems;
     }
 
     @NonNull
     @Override
     public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
 
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.recycler_book_item, parent, false);
         BookViewHolder viewHolder = new BookViewHolder(view);
-
-        viewHolder.mTitle.setText("Testo iniziale");
 
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final BookViewHolder holder, int position) {
         holder.bind(position);
     }
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
 
     @Override
     public int getItemCount() {
         return mNumberItems;
     }
 
-    public class BookViewHolder extends RecyclerView.ViewHolder {
+    public class BookViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        private TextView mTitle;
+        TextView mTitle, mAuthor;
+        ImageView mThumbnail, mOverflow;
 
         public BookViewHolder(View itemView) {
             super(itemView);
 
-            mTitle = itemView.findViewById(R.id.book_title);
+            mTitle = (TextView) itemView.findViewById(R.id.title);
+            mAuthor = (TextView) itemView.findViewById(R.id.author);
+            mThumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            mOverflow = (ImageView) itemView.findViewById(R.id.overflow);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(v.getContext(), "Sono il numero "+getAdapterPosition(), Toast.LENGTH_SHORT).show();
-                }
-            });
+            itemView.setOnClickListener(this);
         }
 
         void bind(int listIndex)
         {
-            mTitle.setText("Position "+listIndex);
+            mTitle.setText("Titolo mediamente lungo");
+            mAuthor.setText("Gino Pilotino Il Terzo");
+            Glide.with(mContext).load(mContext.getResources().getDrawable(R.drawable.book_placeholder_thumbnail)).into(mThumbnail);
+            mOverflow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showPopupMenu(mOverflow);
+                }
+            });
         }
 
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(mContext, "Sono il numer "+getAdapterPosition(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(view.getContext(), view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.book_item, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }
+
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_view_preview:
+                    Toast.makeText(mContext, "Preview", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_add_wishlist:
+                    Toast.makeText(mContext, "Wishlist", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
     }
 }
