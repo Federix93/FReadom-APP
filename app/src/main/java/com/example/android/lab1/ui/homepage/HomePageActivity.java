a88f5f71694326fcd1e803dc07c42ecd4b289eb0
Federico <federico.rizzo93@gmail.com>
2018-04-29 23:49:15 +0200

    Make app all white changing color of toolbar and status bar

--- app/src/main/java/com/example/android/lab1/ui/homepage/HomePageActivity.java
+++ app/src/main/java/com/example/android/lab1/ui/homepage/HomePageActivity.java
@@ -64,6 +64,23 @@ public class HomePageActivity extends AppCompatActivity
 
         Toolbar toolbar = findViewById(R.id.toolbar_home_page_activity);
 
+        Window window = getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.background_app));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decor = getWindow().getDecorView();
+
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+
+        }
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
