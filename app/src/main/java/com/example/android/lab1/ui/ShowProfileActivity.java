b4a9b4a2c37c0c3c4e93f69f83a614bdeb1d7f27
vinsavares <vinsavares@gmail.com>
2018-05-01 23:01:48 +0200

    Implement:  - Profile page layout (top part including photo, informations)  - Partial code for recycler view book items in the profile activity

---
+++ app/src/main/java/com/example/android/lab1/ui/ShowProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.lab1.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.lab1.R;
+
+public class ShowProfileActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_profile);
+    }
+}
