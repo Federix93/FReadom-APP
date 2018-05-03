dd2e0fe139b3c425bec336d2d74bdef048f134ad
vinsavares <vinsavares@gmail.com>
2018-05-03 11:57:53 +0200

    Restyle bookDetail activity

--- app/src/main/java/com/example/android/lab1/ui/BookDetailsActivity.java
+++ app/src/main/java/com/example/android/lab1/ui/BookDetailsActivity.java
@@ -2,27 +2,84 @@ package com.example.android.lab1.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.android.lab1.R;
 import com.example.android.lab1.model.BookPhoto;
 
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
+
 public class BookDetailsActivity extends AppCompatActivity {
 
+    ImageView mImageView;
+    ConstraintLayout mProfileConstraintLayout;
+    Toolbar mToolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_details);
+        Window window = getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.background_app));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decor = getWindow().getDecorView();
+
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        mToolbar = findViewById(R.id.toolbar_book_detail);
+
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        mImageView = findViewById(R.id.profile_image_book_detail);
+
+        mProfileConstraintLayout = findViewById(R.id.profile_detail);
+
+        mProfileConstraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GlobalShowProfileActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+            }
+        });
+
+        Glide.with(this).load(R.mipmap.profile_picture)
+                .apply(bitmapTransform(new CircleCrop()))
+                .into(mImageView);
 
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 3);
         RecyclerView recyclerView = findViewById(R.id.rv_images);
--- app/src/main/java/com/example/android/lab1/ui/GlobalShowProfileActivity.java
+++ app/src/main/java/com/example/android/lab1/ui/GlobalShowProfileActivity.java
@@ -1,25 +1,64 @@
 package com.example.android.lab1.ui;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.example.android.lab1.R;
 import com.example.android.lab1.adapter.ProfileBookAdapter;
 import com.example.android.lab1.model.Book;
 
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
+
 public class GlobalShowProfileActivity extends AppCompatActivity{
 
+    ImageView mImageView;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global_show_profile);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        Window window = getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.background_app));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decor = getWindow().getDecorView();
+
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        mImageView = findViewById(R.id.profile_image);
+
         RecyclerView recyclerView = findViewById(R.id.rv_books);
+
+        Glide.with(this).load(R.mipmap.profile_picture)
+                .apply(bitmapTransform(new CircleCrop()))
+                .into(mImageView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setNestedScrollingEnabled(false);
