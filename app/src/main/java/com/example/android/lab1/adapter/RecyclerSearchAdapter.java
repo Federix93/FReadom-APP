ac6133210c78f4878949798f4154c33a87ca9341
mdp11 <mattia.dipi@gmail.com>
2018-05-03 12:43:52 +0200

    Add and populate recycler view Fix searchview

---
+++ app/src/main/java/com/example/android/lab1/adapter/RecyclerSearchAdapter.java
@@ -0,0 +1,111 @@
+package com.example.android.lab1.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.android.lab1.R;
+import com.example.android.lab1.model.Book;
+import com.example.android.lab1.model.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class RecyclerSearchAdapter extends RecyclerView.Adapter<RecyclerSearchAdapter.ResultViewHolder> {
+
+    private JSONArray mSearchResults;
+    private FirebaseFirestore mFirebaseFirestore;
+
+    public RecyclerSearchAdapter(JSONArray results)
+    {
+        mSearchResults = results;
+        mFirebaseFirestore =  FirebaseFirestore.getInstance();
+    }
+
+    @NonNull
+    @Override
+    public ResultViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.book_search_item, parent, false);
+        return new ResultViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ResultViewHolder holder, int position) {
+        Log.d("TESTBOOK", "Sto a "+position);
+        holder.bind(mSearchResults.optJSONObject(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d("TESTBOOK", String.valueOf(mSearchResults.length()));
+        return mSearchResults.length();
+    }
+
+    public class ResultViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView mTitle, mAuthor, mRating;
+        ImageView mThumbnail, mUserPicture;
+        String mBookId;
+
+        public ResultViewHolder(View itemView) {
+            super(itemView);
+
+            mTitle = itemView.findViewById(R.id.search_book_title);
+            mAuthor = itemView.findViewById(R.id.search_book_author);
+            mThumbnail = itemView.findViewById(R.id.search_book_thumbnail);
+            mUserPicture = itemView.findViewById(R.id.search_book_user_picture);
+            mRating = itemView.findViewById(R.id.search_book_user_rating);
+
+            itemView.setOnClickListener(this);
+        }
+
+        void bind(JSONObject book)
+        {
+            mTitle.setText(book.optString("title"));
+            mAuthor.setText(book.optString("author"));
+            if(book.optString("thumbnail").isEmpty())
+                Glide.with(itemView.getContext()).load(R.drawable.book_placeholder_thumbnail).into(mThumbnail);
+            else
+                Glide.with(itemView.getContext()).load(book.optString("thumbnail")).into(mThumbnail);
+
+            if(!book.optString("uid").isEmpty()) {
+
+                DocumentReference mDocRef = mFirebaseFirestore.collection("users").document(book.optString("uid"));
+                mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        User mUser = documentSnapshot.toObject(User.class);
+                        Glide.with(itemView.getContext()).load(mUser.getImage()).apply(RequestOptions.circleCropTransform()).into(mUserPicture);
+                        mRating.setText(String.format(itemView.getContext().getResources().getConfiguration().locale, "%.1f", mUser.getRating()));
+                    }
+                });
+            }
+
+            mBookId = book.optString("objectID");
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(v.getContext(), "Function not implemented", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/android/lab1/ui/searchbooks/SearchBookActivity.java
+++ app/src/main/java/com/example/android/lab1/ui/searchbooks/SearchBookActivity.java
@@ -4,15 +4,28 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.Toast;
 
+import com.algolia.search.saas.AlgoliaException;
+import com.algolia.search.saas.Client;
+import com.algolia.search.saas.CompletionHandler;
+import com.algolia.search.saas.Index;
+import com.algolia.search.saas.Query;
 import com.arlib.floatingsearchview.FloatingSearchView;
 import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 import com.example.android.lab1.R;
+import com.example.android.lab1.adapter.RecyclerSearchAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SearchBookActivity extends AppCompatActivity {
 
@@ -22,6 +35,9 @@ public class SearchBookActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private FloatingSearchView mSearchView;
+    private Index index;
+    private RecyclerView mRecyclerView;
+    private RecyclerSearchAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +45,14 @@ public class SearchBookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_book);
 
         mSearchView = findViewById(R.id.floating_search_view);
+        mRecyclerView = findViewById(R.id.results_recycler_view);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        Client client = new Client(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY);
+        index = client.getIndex(ALGOLIA_INDEX_NAME);
 
         mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
             @Override
@@ -38,7 +62,6 @@ public class SearchBookActivity extends AppCompatActivity {
 
             @Override
             public void onSearchAction(String currentQuery) {
-                Toast.makeText(getApplicationContext(), currentQuery, Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -46,19 +69,23 @@ public class SearchBookActivity extends AppCompatActivity {
         mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
             @Override
             public void onSearchTextChanged(String oldQuery, String newQuery) {
-                Toast.makeText(getApplicationContext(), oldQuery+">"+newQuery, Toast.LENGTH_SHORT).show();
+
+                if(newQuery.isEmpty())
+                    return;
+
+                Query query = new Query();
+                query.setQuery(newQuery);
+
+                index.searchAsync(query, new CompletionHandler() {
+                    @Override
+                    public void requestCompleted(JSONObject result, AlgoliaException error) {
+                        JSONArray hits = result.optJSONArray("hits");
+                        mAdapter = new RecyclerSearchAdapter(hits);
+                        mRecyclerView.setAdapter(mAdapter);
+                    }
+                });
             }
         });
-
-
-
-
-//        searcher = Searcher.create(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY, ALGOLIA_INDEX_NAME);
-//
-//        searcher.search();
-
-//        InstantSearch helper = new InstantSearch(this, searcher);
-//        helper.search();
 
 
     }
@@ -71,10 +98,7 @@ public class SearchBookActivity extends AppCompatActivity {
         return true;
     }
 
-
-
     protected void onDestroy() {
-      //  searcher.destroy();
         super.onDestroy();
     }
 }
