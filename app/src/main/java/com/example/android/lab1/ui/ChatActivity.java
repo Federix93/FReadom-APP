eff7e3ff23488088e9564b109a838ed52386015b
Federico <federico.rizzo93@gmail.com>
2018-05-11 12:35:32 +0200

    Add chatActivity to project

---
+++ app/src/main/java/com/example/android/lab1/ui/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.lab1.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.lab1.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
