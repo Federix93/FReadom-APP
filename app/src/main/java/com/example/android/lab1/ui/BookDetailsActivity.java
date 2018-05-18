370ff960e7ea1bdb2705a9711f9be2984ef75e2c
Federico <federico.rizzo93@gmail.com>
2018-05-18 16:17:23 +0200

    Add not null annotation

--- app/src/main/java/com/example/android/lab1/ui/BookDetailsActivity.java
+++ app/src/main/java/com/example/android/lab1/ui/BookDetailsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
