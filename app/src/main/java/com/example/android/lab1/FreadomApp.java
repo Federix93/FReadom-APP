78ba9e60aac7ba7711c9b91d0fd1f7ad6fb8db49
mdp11 <mattia.dipi@gmail.com>
2018-05-28 17:28:09 +0200

    Add firebase persistance to Application

--- app/src/main/java/com/example/android/lab1/FreadomApp.java
+++ app/src/main/java/com/example/android/lab1/FreadomApp.java
@@ -3,12 +3,14 @@ package com.example.android.lab1;
 import android.app.Application;
 
 import com.example.android.lab1.utils.LifecycleHandler;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FreadomApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         registerActivityLifecycleCallbacks(new LifecycleHandler());
     }
 }
