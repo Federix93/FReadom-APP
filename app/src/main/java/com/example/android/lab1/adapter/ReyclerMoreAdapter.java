0b99e33e1d72d53e21356ec6cffb48b9a5ee5acb
mdp11 <mattia.dipi@gmail.com>
2018-06-02 00:51:33 +0200

    Develop moreActivity

---
+++ app/src/main/java/com/example/android/lab1/adapter/ReyclerMoreAdapter.java
@@ -0,0 +1,202 @@
+package com.example.android.lab1.adapter;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.android.lab1.R;
+import com.example.android.lab1.model.Book;
+import com.example.android.lab1.ui.BookDetailsActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReyclerMoreAdapter extends RecyclerView.Adapter<ReyclerMoreAdapter.BookViewHolder> {
+
+    private List<Book> books;
+    private Integer mAnimationDuration;
+
+    public ReyclerMoreAdapter(List<Book> books) {
+        if (this.books == null) {
+            this.books = new ArrayList<>();
+        }
+        for (Book b : books) {
+            if (!b.getUid().equals(FirebaseAuth.getInstance().getUid())) {
+                this.books.add(b);
+            }
+        }
+
+    }
+
+    public void updateItems(List<Book> books) {
+        this.books = books;
+    }
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.recycler_book_item, parent, false);
+        return new BookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final BookViewHolder holder, int position) {
+        /*if (mFilteredIds != null && mFilteredIds.contains(IDs.get(position)))
+            holder.layoutHide();
+        else */
+        holder.bind(books.get(position));
+        setFadeAnimation(holder.mRootView);
+
+    }
+
+    private void setFadeAnimation(View view) {
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+        if (mAnimationDuration == null)
+            mAnimationDuration = view.getContext().getResources().getInteger(R.integer.homepage_recycler_view_animation_duration);
+        anim.setDuration(mAnimationDuration);
+        view.startAnimation(anim);
+    }
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+
+    @Override
+    public int getItemCount() {
+        if (books == null)
+            return 0;
+        return books.size();
+    }
+
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+
+        View mRootView;
+        TextView mTitle, mAuthor;
+        ImageView mThumbnail;
+        StorageReference mStorageReference;
+        private ViewGroup.LayoutParams mOldParams;
+
+        public BookViewHolder(final View itemView) {
+            super(itemView);
+            mRootView = itemView;
+            mTitle = itemView.findViewById(R.id.title);
+            mAuthor = itemView.findViewById(R.id.author);
+            mThumbnail = itemView.findViewById(R.id.thumbnail);
+            //mOverflow = itemView.findViewById(R.id.overflow);
+
+        }
+
+        void bind(final Book book) {
+           /* mRootView.setVisibility(View.VISIBLE);
+            if (mOldParams != null) mRootView.setLayoutParams(mOldParams); */
+            mTitle.setText(book.getTitle());
+            mAuthor.setText(book.getAuthors());
+            if (book.getWebThumbnail() != null) {
+                Glide.with(itemView.getContext()).load(book.getWebThumbnail()).into(mThumbnail);
+            } else if (book.getUserBookPhotosStoragePath() != null && !book.getUserBookPhotosStoragePath().isEmpty()) {
+                mStorageReference = FirebaseStorage.getInstance().getReferenceFromUrl(book.getUserBookPhotosStoragePath().get(0));
+                Glide.with(itemView.getContext()).load(mStorageReference).into(mThumbnail);
+            } else
+                Glide.with(itemView.getContext()).load(itemView.getResources().getDrawable(R.drawable.ic_no_book_photo)).into(mThumbnail);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), BookDetailsActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                    intent.putExtra("BookSelected", book);
+                    v.getContext().startActivity(intent);
+                }
+            });
+
+            /*mOverflow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showPopupMenu(mOverflow);
+                }
+            });*/
+        }
+
+        /*private void layoutHide() {
+            mOldParams = mRootView.getLayoutParams();
+            mRootView.setVisibility(View.GONE);
+            mRootView.setLayoutParams(new RecyclerView.LayoutParams(0, 0));
+        } */
+    }
+
+
+    /*private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(view.getContext(), view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.book_item, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }*/
+
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_view_preview:
+                    return true;
+                case R.id.action_add_wishlist:
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+
+    public void addAll(List<Book> newBooks) {
+        int initialSize = books.size();
+        books.addAll(newBooks);
+        notifyItemRangeInserted(initialSize, books.size());
+    }
+
+    public String getLastItemId() {
+        return books.get(books.size() - 1).getBookID();
+    }
+
+    /*private class DownLoadImageTask extends AsyncTask<String, Void, Bitmap> {
+        // TODO move this inner class somewhere else
+        private ImageView mTarget;
+
+        public DownLoadImageTask(ImageView target) {
+            mTarget = target;
+        }
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            Bitmap logo = null;
+            try {
+                InputStream is = new URL(strings[0]).openStream();
+                    //decodeStream(InputStream is)
+                      //  Decode an input stream into a bitmap.
+                logo = BitmapFactory.decodeStream(is);
+            } catch (Exception e) { // Catch the download exception
+                e.printStackTrace();
+            }
+            return logo;
+        }
+        protected void onPostExecute(Bitmap result) {
+            mTarget.setImageBitmap(result);
+        }
+    }*/
+}
--- app/src/main/java/com/example/android/lab1/ui/homepage/MoreActivity.java
+++ app/src/main/java/com/example/android/lab1/ui/homepage/MoreActivity.java
@@ -4,13 +4,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 
 import com.example.android.lab1.R;
-import com.example.android.lab1.adapter.RecyclerBookAdapter;
+import com.example.android.lab1.adapter.ReyclerMoreAdapter;
 import com.example.android.lab1.model.Book;
 import com.example.android.lab1.utils.Utilities;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,9 +28,11 @@ import java.util.List;
 public class MoreActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
-    private RecyclerBookAdapter mAdapter;
+    private ReyclerMoreAdapter mAdapter;
     private GeoPoint mCurrentPosition;
     private Toolbar mToolbar;
+    private AppCompatButton button;
+    private String mNodeID = null;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -38,6 +42,7 @@ public class MoreActivity extends AppCompatActivity {
 
         mToolbar = findViewById(R.id.toolbar_more_activity);
         mRecyclerView = findViewById(R.id.rv_more);
+        button = findViewById(R.id.button_click);
 
         Utilities.setupStatusBarColor(this);
         mToolbar.setTitle("Nelle tue vicinanze");
@@ -50,16 +55,24 @@ public class MoreActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(layoutManager);
 
-        queryDatabase(mCurrentPosition, null);
+        firstQuery(mCurrentPosition);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("GNIPPO", "onClick: LastItemID= "+mAdapter.getLastItemId());
+                queryDatabase(mCurrentPosition, mAdapter.getLastItemId());
+            }
+        });
 
     }
 
-    public void queryDatabase(GeoPoint currentPosition, String nodeID) {
+    public void firstQuery(GeoPoint currentPosition) {
         GeoPoint[] geoPoint = Utilities.buildBoundingBox(currentPosition.getLatitude(),
                 currentPosition.getLongitude(),
                 (double) 15000);
         Query query = FirebaseFirestore.getInstance().collection("books").whereGreaterThan("geoPoint", geoPoint[0])
-                .whereLessThan("geoPoint", geoPoint[1]).limit(30);
+                .whereLessThan("geoPoint", geoPoint[1]).orderBy("geoPoint").limit(3);
         query.get().addOnSuccessListener(this, new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
@@ -70,10 +83,37 @@ public class MoreActivity extends AppCompatActivity {
                             books.add(b);
                         }
                     }
-                    Log.d("GNIPPO", "onSuccess: "+books);
-                    mAdapter = new RecyclerBookAdapter(books);
+                    mAdapter = new ReyclerMoreAdapter(books);
                     mRecyclerView.setAdapter(mAdapter);
                     mRecyclerView.smoothScrollToPosition(0);
+
+                }
+            }
+        });
+    }
+
+    public void queryDatabase(GeoPoint currentPosition, String nodeID) {
+        GeoPoint[] geoPoint = Utilities.buildBoundingBox(currentPosition.getLatitude(),
+                currentPosition.getLongitude(),
+                (double) 15000);
+        Log.d("GNIPPO", "Buildo la query");
+        Query query = FirebaseFirestore.getInstance().collection("books").whereGreaterThan("geoPoint", geoPoint[0])
+                .whereLessThan("geoPoint", geoPoint[1]).orderBy("geoPoint").startAt(nodeID).limit(3);
+        Log.d("GNIPPO", "Lancio la query");
+        query.get().addOnSuccessListener(this, new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                Log.d("GNIPPO", "Onsuccess!!!");
+                List<Book> books = new ArrayList<>();
+                if (queryDocumentSnapshots != null) {
+                    for (Book b : queryDocumentSnapshots.toObjects(Book.class)) {
+                        if (!b.getUid().equals(FirebaseAuth.getInstance().getUid()) /*&& b.getLoanStart() != -1*/) {
+                            books.add(b);
+                            Log.d("GNIPPO", "onSuccess: "+b.getTitle());
+                        }
+                    }
+                    mAdapter.addAll(books);
+                    mAdapter.notifyDataSetChanged();
 
                 }
             }
