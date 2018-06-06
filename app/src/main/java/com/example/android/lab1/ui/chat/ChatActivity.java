4c6a9eb4c4c6b37746d0e81e6ee48b71eab8ead1
Federico <federico.rizzo93@gmail.com>
2018-06-06 17:38:06 +0200

    Fix bug

--- app/src/main/java/com/example/android/lab1/ui/chat/ChatActivity.java
+++ app/src/main/java/com/example/android/lab1/ui/chat/ChatActivity.java
@@ -393,7 +393,7 @@ public class ChatActivity extends AppCompatActivity {
                     mChatArrayAdapter.setItems(messages);
                     mChatArrayAdapter.notifyDataSetChanged();
                 }
-                if(messages.get(messages.size() -1).getSenderId().equals(FirebaseAuth.getInstance().getUid())){
+                if(!messages.get(messages.size() -1).getSenderId().equals(FirebaseAuth.getInstance().getUid())){
                     mChatsReference.child(mChatID).child("counter").setValue(0);
                 }
                 mMessagesRecyclerView.smoothScrollToPosition(mChatArrayAdapter.getItemCount());
